a62a429fc914bad028addfa6bf486292460e0332
AkashSkySingh <AkashpreetSingh91@gmail.com>
2017-03-28 11:12:55 -0700

    Canvas introduction and typo revisions

--- webpack.config.js
+++ webpack.config.js
@@ -5,18 +5,6 @@ module.exports = {
   output: {
     filename: './assets/javascripts/bundle.js'
   },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/, /\.js?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      }
-    ]
-  },
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '*']
