63ee5399a89c605ed1b667e27301e2e0395a916e
AkashSkySingh <AkashpreetSingh91@gmail.com>
2017-03-29 20:47:59 -0700

    require further sound byte editing for sounds

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './lib/inifiniwars.js',
+  entry: './lib/infiniwars.js',
   output: {
     filename: './assets/javascripts/bundle.js'
   },
